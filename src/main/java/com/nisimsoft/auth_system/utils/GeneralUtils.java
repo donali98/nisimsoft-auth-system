411d1d706552711828e8840a039dcadd278e31cc
Donalí <00084417@uca.edu.sv>
2025-06-20 16:14:42 -0600

    refactor: allowing some endpoints temporally

--- src/main/java/com/nisimsoft/auth_system/utils/GeneralUtils.java
+++ src/main/java/com/nisimsoft/auth_system/utils/GeneralUtils.java
@@ -2,6 +2,7 @@ package com.nisimsoft.auth_system.utils;
 
 public class GeneralUtils {
   public static final String[] EXCLUDED_PATHS = {
-    "/api/login", "/api/public/**", "/api/verify-user", "/api/register", "/api/corporation"
+      "/api/login", "/api/public/**", "/api/verify-user", "/api/users", "/api/corporation", "/api/roles",
+      "/api/permissions", "api/users"
   };
 }
